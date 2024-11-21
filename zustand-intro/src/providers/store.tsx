5947b88cbd641e81c6eec065b673044281ad69f7
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-11-21 15:33:15 +0330

    refactor: Clean-up the useStore function of the zustand

--- zustand-intro/src/providers/store.tsx
+++ zustand-intro/src/providers/store.tsx
@@ -1,30 +1,15 @@
 import { create } from "zustand";
 
 type StoreType = {
-  count: number;
-  increment: () => void;
-  increment_by_amount: (value: number) => void;
-  decrement: () => void;
-};
+    count: number;
+    increment: () => void;
+    incrementByAmount: (value: number) => void;
+    decrement: () => void;
+}
 
 export const useStore = create<StoreType>((set) => ({
-  count: 0,
-  increment() {
-    set((state) => ({
-      ...state,
-      count: state.count + 1,
-    }));
-  },
-  increment_by_amount(value) {
-    set((state) => ({
-      ...state,
-      count: state.count + value,
-    }));
-  },
-  decrement() {
-    set((state) => ({
-      ...state,
-      count: state.count - 1,
-    }));
-  },
-}));
+    count: 0,
+    increment: () => set(state => ({ count: state.count + 1 })),
+    incrementByAmount: (value) => set(state => ({...state, count: state.count + value})),
+    decrement: () => set(state => ({ count: state.count - 1 }))
+}))
